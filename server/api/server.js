5bd1ac1ca64ce79f794020d514b662a88c9b52c9
midrra <el.mga.su@gmail.com>
2026-01-23 13:49:47 +0200

    the backed is ready to deploy on vercel

--- server/api/server.js
+++ server/api/server.js
@@ -2,17 +2,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 import express from "express";
 import cors from "cors";
-import dashboardRoute from "./routes/dashboardRoute.js";
+import dashboardRoute from "../routes/dashboardRoute.js";
 import multer from "multer";
 import path from "path";
-import {MainDB} from "./config/db.js";
-import { loginDB } from "./config/loginDB.js";
+import {MainDB} from "../config/db.js";
+import { loginDB } from "../config/loginDB.js";
 
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import authRoutes from "./routes/authRoutes.js";
-import homeRotues from "./routes/homeRoutes.js";
-import captch from "./routes/captch.js"
+import authRoutes from "../routes/authRoutes.js";
+import homeRotues from "../routes/homeRoutes.js";
+import captch from "../routes/captch.js"
 
 const app = express();
 
